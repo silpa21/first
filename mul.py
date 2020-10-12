533988aa1458a9a252d593c82c76a9b771f733d2
silpmarella <44215913+silpmarella@users.noreply.github.com>
2020-10-12 10:57:10 -0400

    Initial commit

---
+++ mul.py
@@ -0,0 +1,4 @@
+import number
+
+my_number = number.Number(5)
+print(my_number.is_even())
---
+++ number.py
@@ -0,0 +1,7 @@
+class Number:
+    def __init__(self, number):
+        self.number = number
+
+    def is_even(self):
+        return self.number % 2 == 0
+
